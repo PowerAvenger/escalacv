a4abda3c921c88c281174aa48edbf747e6d6110c
Jose <>
2024-07-04 08:00:16 +0200

    añadido copyright

--- escalacv.py
+++ escalacv.py
@@ -2,7 +2,7 @@ import streamlit as st
 from backend import graf_ecv_anual, graf_dia
 
 st.set_page_config(layout='wide')
-st.title('Escala Cavero-Vidal')
+st.title('Escala Cavero-Vidal :copyright:')
 
 graf1_año=graf_ecv_anual()
 st.plotly_chart(graf1_año)
