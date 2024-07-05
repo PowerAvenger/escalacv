61dc0cefde12f4fe4f3fae16ede2dc3a9bd37e6d
Jose <>
2024-07-05 09:34:00 +0200

    nuevos graficos

--- backend.py
+++ backend.py
@@ -79,9 +79,14 @@ datos['año']=datos['datetime'].dt.year
 datos.set_index('datetime', inplace=True)
 datos
 
+# %% [markdown]
+# ### Agrupación por días
+
 # %%
 datos_dia=datos.resample('D').mean()
 datos_dia['value']=datos_dia['value'].round(2)
+datos_dia=datos_dia.drop(columns=['hora'])
+datos_dia[['dia','mes','año']]=datos_dia[['dia','mes','año']].astype(int)
 datos_dia
 
 # %%
@@ -89,13 +94,23 @@ valor_minimo_diario=datos_dia['value'].min()
 valor_maximo_diario=datos_dia['value'].max()
 valor_minimo_diario,valor_maximo_diario
 
-# %%
-def graf_año():
-    graf_año=px.line(datos_dia, x='fecha', y='value')
-    return graf_año
+# %% [markdown]
+# ### Agrupación por meses
 
 # %%
-graf_año()
+datos_mes=datos.resample('M').mean()
+datos_mes['value']=datos_mes['value'].round(2)
+datos_mes=datos_mes.drop(columns=['fecha','hora', 'dia'])
+datos_mes[['mes','año']]=datos_mes[['mes','año']].astype(int)
+datos_mes
+
+# %% [markdown]
+# def graf_año():
+#     graf_año=px.line(datos_dia, x='fecha', y='value')
+#     return graf_año
+
+# %% [markdown]
+# ### Definimos la escala y sus límites
 
 # %%
 datos_limites = {
@@ -115,8 +130,15 @@ datos_dia['escala']=pd.cut(datos_dia['value'],bins=df_limites['rango'],labels=et
 datos_dia
 
 # %%
+datos_mes['escala']=pd.cut(datos_mes['value'],bins=df_limites['rango'],labels=etiquetas,right=False)
+datos_mes
+
+# %% [markdown]
+# ### Definimos los colores según la escala
+
+# %%
 colores = {
-    'muy bajo': 'dark green',
+    'muy bajo': 'lightgreen',
     'bajo': 'green',
     'medio': 'blue',
     'alto': 'orange',
@@ -128,23 +150,71 @@ colores = {
 datos_dia['color']=datos_dia['escala'].map(colores)
 datos_dia
 
+# %% [markdown]
+# ### Obtenemos la escala ordenada al reves para el gráfico diario
+
 # %%
-escala_ordenada= ['chungo', 'muy alto','alto','medio','bajo','muy bajo']
-datos_dia['escala']=pd.Categorical(datos_dia['escala'],categories=escala_ordenada, ordered=True)
+escala_dia=datos_dia['escala'].unique()
+escala_dia
+
+# %%
+valor_asignado_a_rango = {row['valor_asignado']: row['rango'] for _, row in df_limites.iterrows()}
+
+# %%
+escala_ordenada_dia = sorted(escala_dia, key=lambda x: valor_asignado_a_rango[x], reverse=True)
+escala_ordenada_dia
+
+# %%
+datos_mes['color']=datos_mes['escala'].map(colores)
+datos_mes
+
+# %% [markdown]
+# ### Obtenemos la escala ordenada al reves para el gráfico mensual
+
+# %%
+escala_mes= datos_mes['escala'].unique()
+escala_mes
+
+# %%
+escala_ordenada_mes = sorted(escala_mes, key=lambda x: valor_asignado_a_rango[x], reverse=True)
+escala_ordenada_mes
+
+# %% [markdown]
+# ### Esta tabla se usa para el gráfico grande de barras diario
+
+# %%
+
+datos_dia['escala']=pd.Categorical(datos_dia['escala'],categories=escala_ordenada_dia, ordered=True)
 datos_dia
+
+# %% [markdown]
+# ### Estos datos se usan para el grafico de barras mensual
+
+# %%
+datos_mes['escala']=pd.Categorical(datos_mes['escala'],categories=escala_ordenada_mes, ordered=True)
+datos_mes
+
+# %% [markdown]
+# ### Estos datos se usan para el quesito
+
+# %%
+datos_dia_queso=datos_dia.groupby(['escala'])['escala'].count()
+datos_dia_queso=datos_dia_queso.reset_index(name='num_dias')
+datos_dia_queso
+
+
+# %% [markdown]
+# ### Gráfico de barras principal
 
 # %%
 def graf_ecv_anual():
     graf_ecv_anual=px.bar(datos_dia, x='fecha', y='value', 
         color='escala',
         color_discrete_map=colores,
-        category_orders={'escala':escala_ordenada},
+        category_orders={'escala':escala_ordenada_dia},
         labels={'value':'precio medio diario €/MWh', 'escala':'escala_cv'},
         title="Precios medios del mercado diario OMIE. Año 2024")
     graf_ecv_anual.update_xaxes(
-        #tickmode='array',
-        #tickvals=pd.date_range(start='2024-01-01', periods=20, freq='MS'),  # Marcas mensuales
-        #ticktext=['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
         showgrid=True
     )
     graf_ecv_anual.update_traces(
@@ -155,6 +225,85 @@ def graf_ecv_anual():
 
 # %%
 graf_ecv_anual()
+
+# %%
+def graf_ecv_mensual():
+    graf_ecv_mensual=px.bar(datos_mes, x='mes', y='value',
+        color='escala',
+        color_discrete_map=colores,
+        category_orders={'escala':escala_ordenada_mes},
+        labels={'value':'precio medio mensual €/MWh', 'escala':'escala_cv'},
+        title="Precios medios mensuales. Año 2024"
+        )
+    #graf_ecv_mensual.update_xaxes(
+    #    showgrid=True
+    
+    #graf_ecv_mensual.update_traces(
+    #   marker_line_width=0
+    #)
+
+    return graf_ecv_mensual
+
+# %%
+graf_ecv_mensual()
+
+# %% [markdown]
+# ### Gráfico de queso
+
+# %%
+def graf_ecv_anual_queso():
+    graf_ecv_anual_queso=px.pie(datos_dia_queso, values='num_dias', names='escala',
+        color='escala',
+        color_discrete_map=colores,
+        #marker=dict(colors=colores),
+        category_orders={'escala':escala_ordenada_dia},
+        labels={'num_dias':'num_dias', 'escala':'escala_cv'},
+        title="% y número de días según la Escala CV. Año 2024",
+        width=500
+        )
+    #graf_ecv_anual_queso.update_traces(hoverlabel=dict(font_color='white', bgcolor='blue'))
+    #graf_ecv_anual.update_xaxes(
+        #tickmode='array',
+        #tickvals=pd.date_range(start='2024-01-01', periods=20, freq='MS'),  # Marcas mensuales
+        #ticktext=['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    #    showgrid=True
+    #)
+    #graf_ecv_anual.update_traces(
+    #    marker_line_width=0
+    #)
+
+    return graf_ecv_anual_queso
+
+# %%
+graf_ecv_anual_queso()
+
+# %% [markdown]
+# ### Gráfica horaria anual
+
+# %%
+datos
+
+# %%
+pt_curva_horaria=datos.pivot_table(
+    values='value',
+    index='hora'
+)
+pt_curva_horaria=pt_curva_horaria['value'].round(2)
+pt_curva_horaria=pt_curva_horaria.reset_index()
+
+
+# %%
+pt_curva_horaria
+
+# %%
+def graf_horaria():
+    graf_horaria=px.line(pt_curva_horaria,x='hora',y='value',
+                     title="Precios medios horarios",
+                     labels={'value': '€/MWh'},
+                     width=800
+    )
+    return graf_horaria
+graf_horaria()
 
 # %%
 mes=1
--- escalacv.py
+++ escalacv.py
@@ -1,5 +1,8 @@
 import streamlit as st
-from backend import graf_ecv_anual, graf_dia
+from backend import graf_ecv_anual, graf_ecv_anual_queso, graf_ecv_mensual
+
+#pg = st.navigation([st.Page("escalacv.py"), st.Page("pages/page_1.py")])
+#pg.run()
 
 st.set_page_config(
     page_title="Escala Cavero Vidal",
@@ -7,9 +10,15 @@ st.set_page_config(
     layout='wide',
 )
 st.title('Escala Cavero-Vidal :copyright:')
+st.caption("Basada en los #telepool de Roberto Cavero. Copyright by Jose Vidal :ok_hand:")
 
-graf1_año=graf_ecv_anual()
-st.plotly_chart(graf1_año)
+#with st.sidebar():
+
+#st.page_link("escalacv.py", label="Home", icon="🏠")
+#st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+
+#graf1_año=graf_ecv_anual()
+st.plotly_chart(graf_ecv_anual())
 
 with st.container():
     col1, col2 = st.columns([0.4,0.6])
@@ -23,5 +32,12 @@ with st.container():
                 st.link_button("Telemindex webapp","https://telemindexpy-josevidal.streamlit.app/")
     with col2:
         st.empty()
+
+col5,col6,col7=st.columns(3)
+with col5:
+    st.plotly_chart(graf_ecv_anual_queso())
+with col6:
+    st.plotly_chart(graf_ecv_mensual())
+#st.plotly_chart(graf2_año)
 #graf1_1=graf_dia()
 #st.plotly_chart(graf1_1)
